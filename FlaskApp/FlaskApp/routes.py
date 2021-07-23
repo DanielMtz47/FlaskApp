bee254f6679d9e3565a0c918e9c3a580d783744e
DanielMtz47 <59232870+DanielMtz47@users.noreply.github.com>
2021-07-22 21:14:13 -0500

    api integration on website

--- FlaskApp/FlaskApp/routes.py
+++ FlaskApp/FlaskApp/routes.py
@@ -1,6 +1,8 @@
 from FlaskApp import app
+from flask import request, redirect, url_for
 from flask import render_template
 import os, json
+import requests
 
 import pandas as pd
 import plotly
@@ -31,15 +33,18 @@ def dataPage():
     {'id': 3, 'name': 'Ángel', 'age': '25', 'height': 1.74}]
     return render_template("test.html",  name = "Username", items = items)
 
-@app.route("/plot")
-def plot():
-    df = px.data.iris()
-    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
-                    size='petal_length', hover_data=['petal_width'])
-    return render_template("plot.html",  name = "Username", graph = getGraph(fig))
+@app.route("/tasks")
+def tasks():
+    return render_template("tasks.html")
 
+@app.route("/tasks/subnetRequest" , methods = ['POST', 'GET'])
+def subnetAPICall():
+    ip = request.form['ip']
+    getRequest = requests.get(f'http://localhost:5000/getSubnet/{ip}')
+    print(getRequest.json())
+    return render_template("tasks.html", subnet = getRequest.json()["subnet"])
+    
 # *********************************************************************************************
-
 def getGraph(fig):
     fig.update_layout(paper_bgcolor="rgb(0,0,0,0)",
                       legend = dict(bgcolor = "rgb(252, 252, 252)"),
